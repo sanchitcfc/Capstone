0b5b96b6fb8a60d4d3f85e7fb42dd591072d7224
sanchitcfc <92146367+sanchitcfc@users.noreply.github.com>
2023-05-10 12:48:31 -0800

    Update final.py

--- final.py
+++ final.py
@@ -11,9 +11,15 @@ from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier
 from sklearn.tree import DecisionTreeClassifier
 from imblearn.over_sampling import SMOTE
 import time
+import gdown
 
 
 def db1():
+    file_id = '1kttchmkJ54VxcpyDTpc65jGCz8VNl7Bb'  # Replace with your file ID
+    url = f'https://drive.google.com/uc?id={file_id}'
+    output = 'data.csv'  # Path and name of the output file
+
+    gdown.download(url, output, quiet=False)
     st.write("Read")
     df = pd.read_csv("Friday.csv")
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
